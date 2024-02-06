8595fd01d8d9f77bddf545c1bb18859007b01ca1
davidinmichael <davidinmichael@gmail.com>
2024-02-06 15:41:02 +0100

    Code refactoring

--- region/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path("", RegionList.as_view()),
-
-    # URL for addidng Regions to the DB
-    # path("add-region/", AddRegion.as_view()),
-]
